cc8787260602a06467d5c2e69a53ce392dc81b3e
Adriano Avelino <adriano.avelino@zup.com.br>
2021-12-09 12:10:55 -0300

    adiciona o controller da recarga de celular

---
+++ src/main/java/br/com/zup/academy/contadigital/bff/recarga_celular/RecargaCelularController.java
@@ -0,0 +1,44 @@
+package br.com.zup.academy.contadigital.bff.recarga_celular;
+
+import br.com.zup.academy.contadigital.bff.compartilhada.api_externa.ApiOquestradorCliente;
+import br.com.zup.academy.contadigital.bff.recarga_celular.RecargaCelularRequest;
+import feign.FeignException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/clientes")
+public class RecargaCelularController {
+
+    @Autowired
+    private ApiOquestradorCliente api;
+
+    @PostMapping("/{idCliente/recarga-celular}")
+    public ResponseEntity<?> realizarOperacao(@PathVariable Long idClinte,
+                                              @RequestBody @Valid RecargaCelularRequest request) {
+        try {
+            api.solicitaRecargaCelular(idClinte, request);
+            return ResponseEntity.ok().build();
+
+        } catch (FeignException.BadRequest e) {
+            return ResponseEntity.badRequest().body("Dados inválidos");
+
+        } catch (FeignException.UnprocessableEntity e) {
+            return ResponseEntity.unprocessableEntity().body("Saldo insuficiente");
+
+        } catch (FeignException.NotFound e) {
+            return ResponseEntity.unprocessableEntity().body("Cliente não encontrado");
+
+        }
+        catch (FeignException.ServiceUnavailable e) {
+            return new ResponseEntity<>("Ocorreu uma falha de comunicação com o serviço de recarga.", HttpStatus.SERVICE_UNAVAILABLE);
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Erro inesperado");
+        }
+    }
+}
