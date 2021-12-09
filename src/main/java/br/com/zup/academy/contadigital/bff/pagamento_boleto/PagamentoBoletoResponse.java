6a57e078d9161fe14d9307095817b82880a93c58
zupperraphaelfeitosa <raphael.feitosa@zup.com.br>
2021-12-08 22:55:11 -0300

    adiciona pagamento bolento response

---
+++ src/main/java/br/com/zup/academy/contadigital/bff/pagamento_boleto/PagamentoBoletoResponse.java
@@ -0,0 +1,10 @@
+package br.com.zup.academy.contadigital.bff.pagamento_boleto;
+
+public class PagamentoBoletoResponse {
+
+    private String resposta;
+
+    public String getResposta() {
+        return resposta;
+    }
+}
